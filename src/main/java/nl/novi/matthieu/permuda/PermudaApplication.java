82f1ce156b8c34caf8386ad48e5a47df5c14eda6
MatthieuMosch <mmosch@gmail.com>
2025-11-16 16:38:44 +0100

    init

---
+++ src/main/java/nl/novi/matthieu/permuda/PermudaApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.matthieu.permuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermudaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermudaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/matthieu/permuda/PermudaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.matthieu.permuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermudaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
